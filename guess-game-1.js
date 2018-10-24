49b7c7dd0cfe38d1ef9c8cd3fb438e5dd03008c6
Heather <unphurlng@gmail.com>
2018-10-24 04:35:18 -0700

    added JavaScript - at stopping poing point - not working

---
+++ guess-game-1.js
@@ -0,0 +1,39 @@
+
+var aboutMeForm = document.getElementById('about-me-form');
+var aboutMeResponse = document.getElementById('amount-me-response');
+
+function checkAboutMe() {
+    var elements = aboutMeForm.elements;
+    
+    var colorInput = elements.favColor;
+    var color = colorInput.value;
+
+//    var numInput = elements.favNum;
+//    var color = numInput.value;
+
+//    var schoolInput = elements.favSchool;
+//    var color = schoolInput.value.toLowerCase();
+
+//    var ageInput = elements.grayHair;
+//    var color = ageInput.value;
+    
+//    var heightInput = elements.heightJumped;
+//    var color = heightInput.value;
+
+    var correct = 0;
+
+    if(color.toLowerCase() === 'violet') {
+        console.log('YAY!');
+        aboutMeResponse.textContent = 'correct';
+        correct = correct + 1;
+        color.classList.add('YAY!');
+        color.classList.remove('Nah, try again.');
+    } else if(color !== '') {
+        color.classList.add('Nah, try again.');
+        color.classList.remove('YAY!');
+    }
+
+
+    aboutMeResponse.textContent = 'You got ' + correct + ' out of 2';
+
+}
