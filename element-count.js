3ca52af0076ed20544841cbf82614b0a2a88c2fe
Heather <unphurlng@gmail.com>
2018-10-25 20:44:21 -0700

    added eslint

--- element-count.js
+++ element-count.js
@@ -1,6 +1,18 @@
+/* eslint no-console: "off" */
+/* exported countQuerySelector */
 'use strict';
 
 function countQuerySelector() {
     var checked = document.querySelectorAll('input[name="tagName"]:checked');
-    console.log(checked[0].value);
+    console.log(checked[0].value.length);
+
+    for(var i = 0; i < countQuerySelector.length; i++) {
+        var elementIndex = countQuerySelector[i];
+        var tags = (document.querySelectorAll(elementIndex.value));
+        var tagCount = tags.length;
+        console.log();
+    }
+
+    
 }
+
